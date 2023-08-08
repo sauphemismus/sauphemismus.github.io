7999e116395e1305caff34cb3d4f5094bcf0f821
sauphemismus <109248365+sauphemismus@users.noreply.github.com>
2023-08-08 20:36:15 +0000

    kzv,u

--- chessGPT/script.js
+++ chessGPT/script.js
@@ -82,7 +82,7 @@ function generate() {
     
     var outArray = output.split("*");
     
-    var sauphemismus = outArray[25];
+    var sauphemismus = outArray[26];
     var nice = 120;
     if (sauphemismus.length > nice || sauphemismus.length < 3 || isNaughty(sauphemismus)) {
       generate();
