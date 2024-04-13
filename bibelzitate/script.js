b00ee0e96e9ff90e9ee38208be458e58d3cfd92a
sauphemismus <109248365+sauphemismus@users.noreply.github.com>
2024-04-13 23:13:51 +0000

    small fixes

--- bibelzitate/script.js
+++ bibelzitate/script.js
@@ -101,7 +101,7 @@ function init(){
 
   generate(
     "Jesaia*Jeremia*Baruch*Ezechiel*Daniel*Hosea*Joel*Amos*Obadiah*Jonah*Micah*Nahum*Habakuk*Zephaniah*Haggai*Zacharias*Malachias*Matthäus*Markus*Lukas*Johannes*",
-    21, 1.1, 100, "*", neuerAutor, 100);
+    21, 1.0, 100, "*", neuerAutor, 100);
 
 
   if(background)
